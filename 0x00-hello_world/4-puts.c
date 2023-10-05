94d51d2b39ab616ca7622c913b152000e1b4e16c
programmingwithjerry <eneyellowj@gmail.com>
2023-10-05 18:34:28 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* This is the main function. it does something useful*/
+/**
+ *This is the main function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
