b76a87dbeeeeb91a4610ed26282980e6dae00189
programmingwithjerry <eneyellowj@gmail.com>
2023-10-06 19:57:52 +0000

    good

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,7 +4,7 @@
  * main - prints the lowercase alphabet in reverse
  *
  * order followed by a new line overall
- * 
+ *
  * using putchar only twice
  *
  * Return: Always 0 (Success)
@@ -12,7 +12,7 @@
 int main(void)
 {
 	char rev_char;
-	
+
 	for (rev_char = 'z'; rev_char >= 'a'; rev_char--)
 	{
 		putchar(rev_char);
