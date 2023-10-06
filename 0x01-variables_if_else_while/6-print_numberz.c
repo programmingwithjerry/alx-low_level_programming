c04f3549fea278cf7cc89e7eaab865d5f1a5b25f
programmingwithjerry <eneyellowj@gmail.com>
2023-10-06 19:30:37 +0000

    base numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ * starting from 0,
+ * followed by a new line, using putchar twice only
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n_base_ten;
+
+	for (n_base_ten = 22; n < 32; n_base_ten++)
+	{
+		putchar(n_base_ten);
+	}
+	putchar('\n');
+	return (0);
+}
