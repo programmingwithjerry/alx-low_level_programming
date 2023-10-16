af0ec57438872c673b38b5aeb55ad996d1e60887
programmingwithjerry <eneyellowj@gmail.com>
2023-10-16 18:31:13 +0000

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*Update value through a pointer*/
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reset_to_98 - update value through a pointer
+ * @n: input character
+ * Return: 0 (success).
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+
+int str_len(char *);
+
+char *_strcat(char *, char *);
+
+void reset_to_98(int *);
+
+#endif
