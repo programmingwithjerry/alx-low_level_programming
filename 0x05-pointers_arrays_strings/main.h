1134c35f96ef06e411d3d7e9ff2014107e7ed9b3
programmingwithjerry <eneyellowj@gmail.com>
2023-10-17 19:38:22 +0000

    swapped

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
+#include <stdio.h>
+#include <unistd.h>
 
-int str_len(char *);
-
-char *_strcat(char *, char *);
-
-void reset_to_98(int *);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
