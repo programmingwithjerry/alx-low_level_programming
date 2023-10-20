2622e66bf3612c66c2f12a8e5232ab28c9202136
programmingwithjerry <eneyellowj@gmail.com>
2023-10-20 13:26:57 +0000

    cool

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
  /** 
@@ -6,17 +5,17 @@
   *
   * Return :always 0
   */
-  int main(void)
+int main(void)
 {
-	_putchar('_' );
-	_putchar ('p' );
-	_putchar ('u' );
-	_putchar ('t' );
-	_putchar ('c' );
-	_putchar ('h' );
-	_putchar ('a' );
-	_putchar ('r' );
-	_putchar ('\n');
+	char s[] = "_putchar";
+	int icount = 0;
 
-	return (0) ;
+	while (s[icount] != '\0')
+	{
+		_putchar(s[icount]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
 }
