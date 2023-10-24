9ffd253a921fe0b10aed912ebb7be72ae2a089d0
programmingwithjerry <eneyellowj@gmail.com>
2023-10-24 18:21:48 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,30 @@
 #include "main.h"
 /**
- * _strncat - Write a function that concatenates two strings. it will use at
- * most n bytes from src
+ * _strncat - Write a function that concatenates two strings.
+ * it will use at most n bytes from src
  * @dest: This is the output  dest
  * @src: This is the input src
  * @n: This is the number bytes fro src
- * Return: This is my return
+ *Return: This is my return
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_counter, src_counter;
+	int dest_counter, src_count;
 
 	dest_counter = 0;
+
 	while (dest[dest_counter] != '\0')
 	{
 		dest_counter;
 	}
 
-	for (src_counter = 0; src_counter < n && src[src_counter] != '\0'; src_counter++)
-		{
-			dest[dest_counter] = src[src_counter];
-			dest_counter++;
-		}
-	if (src_counter < n)
+	for (src_count = 0; src_count < n && src[src_count] != '\0'; src_count++)
+	{
+		dest[dest_counter] = src[src_count];
+		dest_counter++;
+	}
+
+	if (src_count < n)
 	{
 		dest[dest_counter] = '\0';
 	}
