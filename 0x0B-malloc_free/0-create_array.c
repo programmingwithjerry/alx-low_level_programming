617d533586982812ac147e792a13f5ded1930281
programmingwithjerry <eneyellowj@gmail.com>
2023-10-30 14:40:13 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,16 +7,14 @@
  *@c: Character to insert
  *Return: NULL if size is zero or if it fails,
  *pointer to array if everything is normal.
-
  *FUNCTIONALITY *
-
  * 1. Check if the size is 0. If it is, we return NULL.
  * 2. Then, allocate memory for the array.
  * 3. If the allocation fails, return NULL.
- * 4. If allocation is succeeful, loop through the array and set each element to the value of c.
+ * 4. If allocation is succeeful, loop through the array
+ * and set each element to the value of c.
  * 5. Finally, we return the array.
  * Time Complexity: O(n)
-
 */
 char *create_array(unsigned int size, char c)
 {
