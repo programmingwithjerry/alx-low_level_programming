060748566864d46b1dc48339948d87c570a7229f
programmingwithjerry <eneyellowj@gmail.com>
2023-11-06 17:07:12 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-main.c
+ * Auth: Egene E. Jeremiah
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was
+ * compiled from, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
