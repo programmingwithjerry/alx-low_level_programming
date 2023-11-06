5eee05e0c17f31eb01132195cb64b32b2684bcd9
programmingwithjerry <eneyellowj@gmail.com>
2023-11-06 16:54:56 +0000

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Auth: Egene E. Jeremiah
+ * Desc: Header file that defines a macro named PI
+ * as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
