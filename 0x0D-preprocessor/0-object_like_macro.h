6524be62f03004d4191e3a6a41daa8790b9a94af
programmingwithjerry <eneyellowj@gmail.com>
2023-11-06 16:51:44 +0000

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Egene E. Jeremiah
+ * Desc: Header file that defines a named macro
+ * SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif  /* end OBJECT_LIKE_MACRO_H */
