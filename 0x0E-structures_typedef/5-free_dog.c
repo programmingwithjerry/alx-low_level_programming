46ea4ceca908ced248febcebd8e00e570333c820
programmingwithjerry <eneyellowj@gmail.com>
2023-11-07 17:30:08 +0000

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,20 +6,18 @@
 */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-    {
-        return;
-    }
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name != NULL)
+	{
+		free(d->name);
+	}
 
-    if (d->name != NULL)
-    {
-        free(d->name);
-    }
-
-    if (d->owner != NULL)
-    {
-        free(d->owner);
-    }
-
-    free(d);
+	if (d->owner != NULL)
+	{
+		free(d->owner);
+	}
+	free(d);
 }
