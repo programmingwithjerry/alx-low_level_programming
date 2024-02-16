01025f57707035ab7128e0ec65c89a8778305ca5
programmingwithjerry <eneyellowj@gmail.com>
2024-02-16 19:51:11 +0000

    main.c and main.h

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - invokes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Expect an infinite loop :(\n");
+
+	i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+	printf("Infinite loop handled! \\o/\n");
+
+	return (0);
+}
