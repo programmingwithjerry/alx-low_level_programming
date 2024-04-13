a8ede70f6fe1c98ad309feb91a0b2aed038cbd8d
programmingwithjerry <eneyellowj@gmail.com>
2024-04-13 15:59:41 +0000

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,27 +5,45 @@
  *
  * @ht: Pointer to the hash table to delete.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp1;
-	hash_node_t *tmp2;
-
+	/* Check if hash table pointer is NULL */
 	if (ht == NULL)
 		return;
 
-	for (i = 0; i < ht->size; i++)
+	/* Declare variables */
+	unsigned long int idx;
+	hash_node_t *current_node;
+	hash_node_t *next_node;
+
+	/* Iterate over the hash table */
+	for (idx = 0; idx < ht->size; idx++)
 	{
-		tmp1 = ht->array[i];
-		while ((tmp2 = tmp1) != NULL)
+		/* Get the head of the linked list at the current index */
+		current_node = ht->array[idx];
+
+		/* Traverse the linked list */
+		while (current_node != NULL)
 		{
-			tmp1 = tmp1->next;
-			free(tmp2->key);
-			free(tmp2->value);
-			free(tmp2);
+			/* Free key and value of current node */
+			free(current_node->key);
+			free(current_node->value);
+
+			/* Save reference to next node */
+			next_node = current_node->next;
+
+			/* Free current node */
+			free(current_node);
+
+			/* Move to the next node */
+			current_node = next_node;
 		}
 	}
+
+	/* Free array of pointers */
 	free(ht->array);
+
+	/* Free hash table itself */
 	free(ht);
 }
+
