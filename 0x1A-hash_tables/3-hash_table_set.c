216664f32f400ec62635ec39bd63263be043bd80
programmingwithjerry <eneyellowj@gmail.com>
2024-04-13 11:23:03 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,53 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * hash_table_set - function that adds an element to the hash table
- * @ht: The hash table
- * @key: The key of the table which can not be an empty string
- * @value: The the value associated with the key
- * Return: 1 on success, 0 on failure
- */
 
+/**
+ * hash_table_set - Inserts or updates a key-value pair in the hash table.
+ *
+ * @ht: Pointer to the hash table.
+ * @key: The key to insert or update.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if successful, 0 otherwise.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	/* Declare variables */
-	unsigned long int index = 0;
-	hash_node_t **tmp_ptr = NULL; /* Double pointer to hash_node_t */
-	hash_node_t *new_hash_node = NULL;
+	unsigned long int idx = 0;
+	hash_node_t *new_node = NULL;
+	hash_node_t *current_node = NULL;
 
-	/* Check for invalid inputs */
+	/* Check for NULL pointers or empty key */
 	if (!ht || !key || !(*key) || !value)
 		return (0);
-
-	/* Calculate index based on key */
-	index = key_index((unsigned char *)key, ht->size);
-	tmp_ptr = &(ht->array[index]); /* Assign address of pointer to tmp_ptr */
-
-	/* Traverse the linked list until the end or matching key is found */
-	while (*tmp_ptr && strcmp((*tmp_ptr)->key, key) != 0)
-		tmp_ptr = &((*tmp_ptr)->next);
-
-	/* If key already exists, update value */
-	if (*tmp_ptr)
+	/* Calculate index for the key */
+	idx = key_index((unsigned char *)key, ht->size);
+	/*// Point to the head of the linked list at index*/
+	current_node = ht->array[idx];
+	/* Search for existing node with the same key */
+	while (current_node && strcmp(current_node->key, key) != 0)
+		current_node = current_node->next;
+	/* If key exists, update its value */
+	if (current_node)
 	{
-		free((*tmp_ptr)->value);
-		(*tmp_ptr)->value = strdup(value);
+		free(current_node->value);
+		current_node->value = strdup(value);
 		return (1);
 	}
-
-	/* Allocate memory for new node */
-	new_hash_node = malloc(sizeof(*new_hash_node));
-	if (!new_hash_node)
+	/* Allocate memory for a new node */
+	new_node = malloc(sizeof(*new_node));
+	if (!new_node)
 		return (0);
-
 	/* Assign key and value to the new node */
-	new_hash_node->key = strdup(key);
-	new_hash_node->value = strdup(value);
-
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	/* Insert new node at the beginning of the linked list */
-	new_hash_node->next = *tmp_ptr; /* Point new node to current head of list*/
-	*tmp_ptr = new_hash_node; /*Update poiter from prev node to point tonew node*/
-
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
 	return (1);
 }
+
