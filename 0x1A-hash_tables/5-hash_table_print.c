00108c96f02afe3b9e9ce901b3586adc2285a87f
programmingwithjerry <eneyellowj@gmail.com>
2024-04-13 15:20:40 +0000

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,32 +6,44 @@
  * @ht: Pointer to the hash table.
  */
 
-
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int idx;
-	hash_node_t *current_node;
-	char *separator;
-
+	/* Check if hash table pointer is NULL */
 	if (ht == NULL)
 		return;
 
-	printf("{");
-	separator = "";
+	/* Declare variables */
+	unsigned long int idx;
+	hash_node_t *c_node;
+	char *sept;
 
+	/* Initialize sept */
+	printf("{");
+	sept = "";
+
+	/* Iterate over the hash table */
 	idx = 0;
 	while (idx < ht->size)
 	{
-		current_node = ht->array[idx];
-		while (current_node != NULL)
+		/* Get the head of the linked list at the current index */
+		c_node = ht->array[idx];
+
+		/* Traverse the linked list */
+		while (c_node != NULL)
 		{
-			printf("%s'%s': '%s'", separator, current_node->key, current_node->value);
-			separator = ", ";
-			current_node = current_node->next;
+			/* Print key-value pair */
+			printf("%s'%s': '%s'", sept, c_node->key, c_node->value);
+
+			/* Update sept */
+			sept = ", ";
+
+			/* Move to the next node */
+			c_node = c_node->next;
 		}
 		idx++;
 	}
 
+	/* Print closing brace and newline */
 	printf("}\n");
 }
-			
+
