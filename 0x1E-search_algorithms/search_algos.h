da6bad08df4ebcaacf7b748adb2f51e3ba3e0dc9
programmingwithjerry <eneyellowj@gmail.com>
2024-07-16 12:39:34 -0700

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -37,6 +37,7 @@ typedef struct skiplist_s
 } skiplist_t;
 
 /*void print_array(int *array, size_t size;*/
+int my_binary_search(int *array, size_t left, size_t right, int value)
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
