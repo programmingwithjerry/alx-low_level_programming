97127e31951107473d2b9349867393f75c389950
programmingwithjerry <eneyellowj@gmail.com>
2024-07-16 06:36:03 -0700

    1-binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -36,6 +36,7 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+void print_array(int *array, size_t size;
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
